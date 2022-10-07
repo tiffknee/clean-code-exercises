b7b76aa3048ee67ce29e6f5931db2a35e169e44c
tiffknee <tiffkneee@gmail.com>
2022-10-07 12:13:47 +0200

    created a dataclass point to make the rastergrid function more readable

--- raster_grid.py
+++ raster_grid.py
@@ -9,6 +9,10 @@
 from typing import Tuple
 from dataclasses import dataclass
 
+@dataclass
+class Point:
+    x: float
+    y: float
 
 class RasterGrid:
     @dataclass
@@ -17,16 +21,15 @@ class RasterGrid:
         _iy: int
 
     def __init__(self,
-                 x0: float,
-                 y0: float,
-                 x1: float,
-                 y1: float,
+                 origin: Point,
+                 size: Tuple[float,float],
                  nx: int,
                  ny: int) -> None:
-        self._x0 = x0
-        self._y0 = y0
-        self._x1 = x1
-        self._y1 = y1
+        self._origin = origin
+        self._extent = Point(
+            self._origin.x + size[0],
+            self._origin.y + size[1]
+            )
         self._nx = nx
         self._ny = ny
         self.nc = nx*ny
@@ -34,45 +37,51 @@ class RasterGrid:
             self.Cell(i, j) for i in range(nx) for j in range(ny)
         ]
 
-    def calculate_new_coordinates(self, cell: Cell) -> Tuple[float, float]:
+    # @property
+    # def cells(self) -> List[Cell]:
+    #     return [
+    #         self.Cell(i, j) for i in range(self._nx) for j in range(self._ny)
+    #     ]
+
+    # @property
+    # def number_of_cells(self) -> int:
+    #     return self._nx*self._ny
+
+    def get_center_coordinates(self, cell: Cell) -> Tuple[float, float]:
         return (
-            self._x0 + (float(cell._ix) + 0.5)*(self._x1 - self._x0)/self._nx,
-            self._y0 + (float(cell._iy) + 0.5)*(self._y1 - self._y0)/self._ny
+            self._origin.x + (float(cell._ix) + 0.5)*(self._extent.x - self._origin.x)/self._nx,
+            self._origin.y + (float(cell._iy) + 0.5)*(self._extent.y - self._origin.y)/self._ny
         )
 
     def get_cell_from_coordinates(self, x: float, y: float) -> Cell:
         eps = 1e-6*max(
-            (self._x1-self._x0)/self._nx,
-            (self._y1-self._y0)/self._ny
+            (self._extent.x-self._origin.x)/self._nx,
+            (self._extent.y-self._origin.y)/self._ny
         )
-        if abs(x - self._x1) < eps:
+        if abs(x - self._extent.x) < eps:
             ix = self._nx - 1
-        elif abs(x - self._x0) < eps:
+        elif abs(x - self._origin.x) < eps:
             ix = 0
         else:
-            ix = int((x - self._x0)/((self._x1 - self._x0)/self._nx))
-        if abs(y - self._y1) < eps:
+            ix = int((x - self._origin.x)/((self._extent.x - self._origin.x)/self._nx))
+        if abs(y - self._extent.y) < eps:
             iy = self._ny - 1
-        elif abs(y - self._y0) < eps:
+        elif abs(y - self._origin.y) < eps:
             iy = 0
         else:
-            iy = int((y - self._y0)/((self._y1 - self._y0)/self._ny))
+            iy = int((y - self._origin.y)/((self._extent.y - self._origin.y)/self._ny))
         return self.Cell(ix, iy)
 
 
 def test_number_of_cells():
-    x0 = 0.0
-    y0 = 0.0
-    dx = 1.0
-    dy = 1.0
-    assert RasterGrid(x0, y0, dx, dy, 10, 10).nc == 100
-    assert RasterGrid(x0, y0, dx, dy, 10, 20).nc == 200
-    assert RasterGrid(x0, y0, dx, dy, 20, 10).nc == 200
-    assert RasterGrid(x0, y0, dx, dy, 20, 20).nc == 400
+    assert RasterGrid(Point(0.0,0.0), [1.0,1.0], 10, 10).nc == 100
+    assert RasterGrid(Point(0.0,0.0), [1.0,1.0], 10, 20).nc == 200
+    assert RasterGrid(Point(0.0,0.0), [1.0,1.0], 20, 10).nc == 200
+    assert RasterGrid(Point(0.0,0.0), [1.0,1.0], 20, 20).nc == 400
 
 
 def test_locate_cell():
-    grid = RasterGrid(0.0, 0.0, 2.0, 2.0, 2, 2)
+    grid = RasterGrid(Point(0.0,0.0), [2.0,2.0], 2, 2)
     cell = grid.get_cell_from_coordinates(0, 0)
     assert cell._ix == 0 and cell._iy == 0
     cell = grid.get_cell_from_coordinates(1, 1)
@@ -88,19 +97,19 @@ def test_locate_cell():
 
 
 def test_cell_center():
-    grid = RasterGrid(0.0, 0.0, 2.0, 2.0, 2, 2)
+    grid = RasterGrid(Point(0.0,0.0), [2.0,2.0], 2, 2)
     cell = grid.get_cell_from_coordinates(0.5, 0.5)
-    assert abs(grid.calculate_new_coordinates(cell)[0] - 0.5) < 1e-7 and abs(grid.calculate_new_coordinates(cell)[1] - 0.5) < 1e-7
+    assert abs(grid.get_center_coordinates(cell)[0] - 0.5) < 1e-7 and abs(grid.get_center_coordinates(cell)[1] - 0.5) < 1e-7
     cell = grid.get_cell_from_coordinates(1.5, 0.5)
-    assert abs(grid.calculate_new_coordinates(cell)[0] - 1.5) < 1e-7 and abs(grid.calculate_new_coordinates(cell)[1] - 0.5) < 1e-7
+    assert abs(grid.get_center_coordinates(cell)[0] - 1.5) < 1e-7 and abs(grid.get_center_coordinates(cell)[1] - 0.5) < 1e-7
     cell = grid.get_cell_from_coordinates(0.5, 1.5)
-    assert abs(grid.calculate_new_coordinates(cell)[0] - 0.5) < 1e-7 and abs(grid.calculate_new_coordinates(cell)[1] - 1.5) < 1e-7
+    assert abs(grid.get_center_coordinates(cell)[0] - 0.5) < 1e-7 and abs(grid.get_center_coordinates(cell)[1] - 1.5) < 1e-7
     cell = grid.get_cell_from_coordinates(1.5, 1.5)
-    assert abs(grid.calculate_new_coordinates(cell)[0] - 1.5) < 1e-7 and abs(grid.calculate_new_coordinates(cell)[1] - 1.5) < 1e-7
+    assert abs(grid.get_center_coordinates(cell)[0] - 1.5) < 1e-7 and abs(grid.get_center_coordinates(cell)[1] - 1.5) < 1e-7
 
 
 def test_cell_iterator() -> None:
-    grid = RasterGrid(0.0, 0.0, 2.0, 2.0, 2, 2)
+    grid = RasterGrid(Point(0.0,0.0), [2.0,2.0], 2, 2)
     count = sum(1 for _ in grid.cells)
     assert count == grid.nc
 
